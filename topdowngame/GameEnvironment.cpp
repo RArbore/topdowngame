c081f99ee40fe71e20072e25eced23ca51d881ce
Russel <russel.jma@gmail.com>
2020-03-29 23:13:28 -0700

    Added focus entity health bar
    Usually for player, but if the focus has no health (say it's a cutscene manager) the health bar will just not render

--- topdowngame/GameEnvironment.cpp
+++ topdowngame/GameEnvironment.cpp
@@ -28,15 +28,38 @@ void GameEnvironment::tick() {
 }
 
 void GameEnvironment::render() {
+	sf::View prevView = window->getDefaultView();
 	sf::Vector2f cameraPos(focusEntity->h.getCX(), focusEntity->h.getCY());
 	camera.setCenter(cameraPos);
 	camera.setSize(static_cast<sf::Vector2f>(window->getSize()));
 	window->setView(camera.getView());
 
+	//Draw relative to focus
+
 	window->draw(tileMap);
 
 	for (int i = entities.size() - 1; i >= 0; i--) {
 		entities.at(i)->render(window);
+	}
+
+	//Draw at absolute positions
+
+	prevView.setCenter(sf::Vector2f(0, 0));
+	prevView.setSize(static_cast<sf::Vector2f>(window->getSize()));
+	window->setView(prevView);
+
+	if (focusEntity->maxHealth > 0) {
+		sf::Vector2u size = window->getSize();
+		sf::RectangleShape healthBorder(sf::Vector2f(400, 80));
+		healthBorder.setPosition(sf::Vector2f(-float(size.x) / 2, -float(size.y) / 2));
+		healthBorder.setFillColor(sf::Color(150, 0, 0));
+
+		sf::RectangleShape healthBar(sf::Vector2f(380.f*focusEntity->health/focusEntity->maxHealth, 60));
+		healthBar.setPosition(sf::Vector2f(-float(size.x) / 2 + 10, -float(size.y) / 2 + 10));
+		healthBar.setFillColor(sf::Color(220, 0, 0));
+
+		window->draw(healthBorder);
+		window->draw(healthBar);
 	}
 }
 
